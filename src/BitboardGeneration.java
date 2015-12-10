fc7745f79f51ba679bd2a07b1a3e05fc9f5dbabd
akshayjnambiar <akshayjnambiar@gmail.com>
2015-12-10 10:36:10 +0530

    Created UserInterface class too
    This class has the main method now

--- src/BitboardGeneration.java
+++ src/BitboardGeneration.java
@@ -24,32 +24,41 @@ public class BitboardGeneration {
     private static long K, Q, B, N, R, P, k, q, b, n, r, p;
     private static boolean debug = true;
     
+    /**
+     *  This function is the first function to be called and it initializes the chess board 
+     *  to the standard arrangement.  
+     * 
+     *  @param void
+     *  @return void
+     */
     public static void initializeStandardChessBoard() {
         
         // Initialize the bitboards to O 
         K = Q = B = N = R = P = k = q = b = n = r = p = 0L;
         
-        //  String to represent the standard chessboard
+        //  String to represent the standard chess board
         String zeroes="";
         for(int i=0;i<32;i++) zeroes+="0";
         
         String chessBoard = "rnbqkbnrpppppppp" + zeroes + "PPPPPPPPRNBQKBNR";
-        
-        
-        
+
         //  Convert to bit boards 
         convertToBitboards(chessBoard);
     }
     
+    /**
+     * This function is used to initialize the bitboard variables according to the 
+     * arrangement in the chessBoard string. This will be a 64 bit binary. 
+     * 
+     * @param chessBoard
+     * @return void
+     */
     public static void convertToBitboards(String chessBoard) {
         // Variable used to convert to Bitboards
         String Bits;
         
         for(int i=0;i<64;i++) {
-            
-            if(chessBoard.charAt(i) == ' ') continue;
-            System.out.println(chessBoard.charAt(i));
-            
+        	
             Bits="";
             for(int j=0;j<64;j++){
                 Bits+="0";
@@ -59,18 +68,18 @@ public class BitboardGeneration {
             
             switch(chessBoard.charAt(i)) {
                 
-                case 'K' : K += parseLong(Bits); break;
-                case 'Q' : Q += parseLong(Bits); break;
-                case 'B' : B += parseLong(Bits); break;
-                case 'N' : N += parseLong(Bits); break;
-                case 'R' : R += parseLong(Bits); break;
-                case 'P' : P += parseLong(Bits); break;
-                case 'k' : k += parseLong(Bits); break;
-                case 'q' : q += parseLong(Bits); break;
-                case 'b' : b += parseLong(Bits); break;
-                case 'n' : n += parseLong(Bits); break;
-                case 'r' : r += parseLong(Bits); break;
-                case 'p' : p += parseLong(Bits); break;
+                case 'K' : K += parseLongPositive(Bits); break;
+                case 'Q' : Q += parseLongPositive(Bits); break;
+                case 'B' : B += parseLongPositive(Bits); break;
+                case 'N' : N += parseLongPositive(Bits); break;
+                case 'R' : R += parseLongPositive(Bits); break;
+                case 'P' : P += parseLongPositive(Bits); break;
+                case 'k' : k += parseLongPositive(Bits); break;
+                case 'q' : q += parseLongPositive(Bits); break;
+                case 'b' : b += parseLongPositive(Bits); break;
+                case 'n' : n += parseLongPositive(Bits); break;
+                case 'r' : r += parseLongPositive(Bits); break;
+                case 'p' : p += parseLongPositive(Bits); break;
                 
             }
             
@@ -81,17 +90,26 @@ public class BitboardGeneration {
             }
     }
         
-    private static long parseLong(String s) {
-        if (s.charAt(0)=='0') {//not going to be a negative number
+    /**
+     * This function is used to convert the binary string to a long value 
+     * without getting a negative value (in the case of s[0]=1)
+     * 
+     * @param s(String containing the bitboard representation)
+     * @return long(The long value of the binary representation)
+     */
+    private static long parseLongPositive(String s) {
+        if (s.charAt(0)=='0') {     //not going to be a negative number
             return Long.parseLong(s, 2);
         } else {
             return Long.parseLong("1"+s.substring(2), 2)*2;
         }
     }
     
-    /* 
-     *   Only for debug to print the chessboard. 
-     * 
+    /** 
+     *   Only for debug to print the chessboard.
+     *    
+     *   @param void
+     *   @return void
      */
      public static void printChessBoard() {
          String chessBoard[][]=new String[8][8];
@@ -117,8 +135,5 @@ public class BitboardGeneration {
              System.out.println(Arrays.toString(chessBoard[i]));
          }
      }
-     
-     public static void main(String args) {
-         initializeStandardChessBoard();
-     }
+  
 }
---
+++ src/UserInterface.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of UserInterface here.
+ * 
+ * This class is about the UserInterface.
+ * 
+ * @author   AKSHAY J NAMBIAR 
+ * @version  1.0
+ * @date     12/9/2015
+ */
+
+
+public class UserInterface {
+    public static void main(String[] args) {
+        BitboardGeneration.initializeStandardChessBoard();
+    }
+}
